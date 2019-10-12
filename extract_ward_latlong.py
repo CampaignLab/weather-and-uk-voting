be776b775bb7e378104dc9b5873bc8e322a91bda
karnival <dpdowen@gmail.com>
2019-10-12 15:56:07 +0100

    Fetch weather logs for very coarse map

---
+++ extract_ward_latlong.py
@@ -0,0 +1,35 @@
+import csv
+import json
+
+from tqdm import tqdm
+from datetime import datetime
+
+from weather_lookup import dump_weather_for_t
+
+id_to_weather_fn = {}
+id_to_name = {}
+
+
+with open('geography-data/Wards_December_2018_Super_Generalised_Clipped_Boundaries_GB.csv') as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader)  # skip header
+
+    for row in tqdm(reader):
+        objectid = row[0]
+        id = row[1]
+        name = row[2]
+        longitude = float(row[6])
+        latitude = float(row[7])
+
+        weather_filename = dump_weather_for_t(t=datetime(year=2018, month=5, day=3).isoformat(),
+                                              location=(latitude, longitude),
+                                              dry_run=False)
+
+        id_to_weather_fn[id] = weather_filename
+        id_to_name[id] = name
+
+    with open('map-id-to-weather.json', 'w') as f_js:
+        json.dump(id_to_weather_fn, f_js)
+
+    with open('map-id-to-name.json', 'w') as f_js:
+        json.dump(id_to_name, f_js)
--- weather_lookup.py
+++ weather_lookup.py
@@ -1,24 +1,59 @@
 import os
 import json
+import glob
+import numpy as np
+from collections import defaultdict
+from tqdm import tqdm
 from darksky import forecast
 from datetime import datetime
 
 key = os.environ['DARKSKY_KEY']
+saved_weather_fns = glob.glob('weather-data-output/*/*.json')
+saved_weather = defaultdict(list)
+
+for fn in saved_weather_fns:
+    t = fn.split('/')[1]
+    lat = float(fn.split('/')[2].split('_')[-2])
+    long = float(fn.split('/')[2].split('_')[-1][:-5])
+    saved_weather[t].append([lat, long])
 
 
 def dump_weather_for_t(t: datetime = datetime(year=2018, month=5, day=3).isoformat(),
-                       locations: tuple = ((42.36, -71.05),),
+                       location: tuple = (42.36, -71.05),
                        dry_run=True):
-    for loc in locations:
-        if not dry_run:
-            out = forecast(key, loc[0], loc[1], time=t)
+    if not os.path.isdir(f'weather-data-output/{t}'):
+        os.makedirs(f'weather-data-output/{t}')
 
-    out_filename = f'weather-data-output/{t}-{loc}.json'
-    with open(out_filename, 'w') as f:
-        json.dump(out['hourly'], f)
+    needs_new_request = False
+
+    if len(saved_weather[t]) > 0:
+        dists = np.linalg.norm(np.array(saved_weather[t]) - np.array(location), axis=1)
+        dist_min_ind = np.argmin(dists)
+
+        try:
+            dist_min = dists[dist_min_ind]
+        except IndexError:
+            dist_min = dists  # just one entry
+
+        if dist_min > 1:  # 0.1deg is about 7 miles
+            needs_new_request = True
+    else:
+        needs_new_request = True
+
+    if not needs_new_request:
+        coords_cached = saved_weather[t][dist_min_ind]
+        out_filename = f'weather-data-output/{t}/{coords_cached[0]}_{coords_cached[1]}.json'
+    elif needs_new_request and not dry_run:
+        out = forecast(key, latitude=location[0], longitude=location[1], time=t)
+        out_filename = f'weather-data-output/{t}/{location[0]}_{location[1]}.json'
+
+        with open(out_filename, 'w') as f:
+            json.dump(out['hourly'], f)
+
+        saved_weather[t].append([location[0], location[1]])
 
     return out_filename
 
 
 if __name__ == '__main__':
-    dump_weather_for_t()
+    dump_weather_for_t(dry_run=True)
